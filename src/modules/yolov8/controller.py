3c9d08b5265257921bbe18ba1cdb0044d533bd01
Brilhante29 <miudosan80@gmail.com>
2023-10-23 18:42:08 -0300

    add pose

--- src/modules/yolov8/controller.py
+++ src/modules/yolov8/controller.py
@@ -1,18 +1,20 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, HTTPException, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import StreamingResponse
 from src.core.enums.media_types_enum import MediaTypes
-
-from src.modules.yolov8.service import generate_video
+from src.modules.yolov8.service import generate_video, generate_video_pose
 
 templates = Jinja2Templates(directory="src/modules/yolov8/templates")
-
 router = APIRouter(tags=['yolov8'])
 
-@router.get("/video_feed/{source}")
-async def video_feed(source):
-    return StreamingResponse(generate_video(source), media_type=MediaTypes.VIDEO.value)
-
+@router.get("/video_feed/{source_type}/{source}")
+async def video_feed(source_type: str, source: str):
+    if source_type == 'normal':
+        return StreamingResponse(generate_video(source), media_type=MediaTypes.VIDEO.value)
+    elif source_type == 'pose':
+        return StreamingResponse(generate_video_pose(source), media_type=MediaTypes.VIDEO.value)
+    else:
+        raise HTTPException(status_code=400, detail="Invalid source type")
 
 @router.get("/video")
 async def video(request: Request):
--- src/modules/yolov8/service.py
+++ src/modules/yolov8/service.py
@@ -1,11 +1,13 @@
+import asyncio
+import threading
 import cv2
 from ultralytics import YOLO
 from src.core.enums.video_source_types_enum import VideoSourceType
 
 MODEL_PATH = '../../ml/models/yolov8/yolov8n.pt'
+MODEL_PATH_POSE = '../../ml/models/yolov8/yolov8n-pose.pt'
 FRAME_STRING = (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n')
-
 
 def setup_model():
     return YOLO(MODEL_PATH)
@@ -16,12 +18,12 @@ def setup_video_source(source: str):
         VideoSourceType.WEBCAM.value: lambda: cv2.VideoCapture(0),
         VideoSourceType.FILE.value: lambda: cv2.VideoCapture(source)
     }
-
     return video_actions.get(source, lambda: cv2.VideoCapture(source))()
 
 
-def generate_video(source: str):
-    model = setup_model()
+async def generate_video(source: str):
+    loop = asyncio.get_event_loop()
+    model = YOLO(MODEL_PATH)
     cap = setup_video_source(source)
 
     if not cap.isOpened():
@@ -34,10 +36,37 @@ def generate_video(source: str):
             if not ret:
                 break
 
-            results = model.track(frame, persist=True)
-            frame_ = results[0].plot()
-
-            yield FRAME_STRING + cv2.imencode('.jpg', frame_)[1].tobytes() + b'\r\n'
-
+            results = await loop.run_in_executor(None, model.track, frame, True)
+            results_list = list(results)  # Convert generator to list if necessary
+            if results_list:
+                frame_ = results_list[0].plot()
+                yield FRAME_STRING + cv2.imencode('.jpg', frame_)[1].tobytes() + b'\r\n'
     finally:
         cap.release()
+        
+        
+
+async def generate_video_pose(source: str):
+    model = YOLO(MODEL_PATH_POSE)
+    cap = setup_video_source(source)
+
+    if not cap.isOpened():
+        cap.release()
+        raise Exception("Erro ao abrir o arquivo de vídeo")
+
+    try:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            results = model(frame)
+            results_list = list(results)  # Convert generator to list if necessary
+            if results_list:
+                frame_ = results_list[0].plot()
+                yield FRAME_STRING + cv2.imencode('.jpg', frame_)[1].tobytes() + b'\r\n'
+    finally:
+        cap.release()
+
+
+
